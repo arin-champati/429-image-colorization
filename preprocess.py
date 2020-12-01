93d84d69775cf6c88f98d1e3fc306feda069dcbc
Arin Champati <champati.arin@gmail.com>
2020-11-30 21:58:09 -0500

    many changes

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,8 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from skimage.transform import resize
 
+import cielab
+
 DATASET_PATH = './Data/Stanford Dog Dataset'
 PICKLE_PATH = './Data/Preprocessed Data'
 
@@ -21,37 +23,45 @@ def __convert_images_helper(full_filename, size):
     """
     im_color = cv2.imread(full_filename)
 
-    # reverse im_color from BGR to RGB to be passed to rgbtolab
+    # reverse im_color from BGR to RGB
     im_color = im_color[:,:,::-1]
     im_gray = cv2.imread(full_filename, 0)
 
     im_color = cv2.resize(im_color, (size, size), interpolation=cv2.INTER_AREA)
     im_gray = cv2.resize(im_gray, (size, size), interpolation=cv2.INTER_AREA)
 
-    # convert from RGB to LAB color space
-    im_color_lab = color.rgb2lab(im_color)
+    im_color_ab, im_quantized = cielab.preprocess(full_filename, size)
 
     # add extra dimension to front for num images
-    im_color_lab = im_color_lab.reshape((1, size, size, 3))
     im_color = im_color.reshape((1, size, size, 3))
     im_gray = im_gray.reshape((1, size, size, 1))
 
-    return im_color_lab, im_color, im_gray
+    im_quantized = im_quantized.reshape((1, size, size, 313))
+    im_color_ab = im_color_ab.reshape((1, size, size, 2))
+
+    return im_color, im_gray, im_color_ab, im_quantized
 
 def __loop_filetree(image_path, file_list, size):
     """
     Helper to loop over filetree
     """
-    images = []
-    i = 0
-    for file_name in file_list:
+    color_images = np.zeros((len(file_list), size, size, 3))
+    gray_images = np.zeros((len(file_list), size, size, 1))
+    labels_ab = np.zeros((len(file_list), size, size, 2))
+    labels_quantized_ab = np.zeros((len(file_list), size, size, 313))
+
+    for i, file_name in enumerate(file_list):
         file_name = file_name[0][0]
         full_filename = os.path.join(image_path, file_name)
-        im_color_lab, img_color, im_gray = __convert_images_helper(full_filename, size)
+        im_color, im_gray, im_color_ab, im_quantized = __convert_images_helper(full_filename, size)
 
-        images.append({'rgb': img_color, 'lab': im_color_lab, 'gray': im_gray})
+        color_images[i,:,:,:] = im_color
+        gray_images[i,:,:,:] = im_gray
+        labels_ab[i,:,:,:] = im_color_ab
+        labels_quantized_ab[i,:,:,:] = im_quantized
+    
+    return color_images, gray_images, labels_ab, labels_quantized_ab
 
-    return images
 
 def convert_images(image_path, train_label_path, test_label_path, size=64):
     """
@@ -61,43 +71,59 @@ def convert_images(image_path, train_label_path, test_label_path, size=64):
     summary: Creates list of tuples of images and labels
     """
 
-    test_mat_file = scipy.io.loadmat(test_label_path)
-    test_images = __loop_filetree(image_path, test_mat_file['file_list'], size)
-    
-    print('testing done')
-    
     train_mat_file = scipy.io.loadmat(train_label_path)      
-    train_images = __loop_filetree(image_path, train_mat_file['file_list'], size)
+    train_color, train_gray, train_ab, train_quantized = __loop_filetree(image_path, train_mat_file['file_list'], size)
 
     print('training done')
 
-    return train_images, test_images
+    test_mat_file = scipy.io.loadmat(test_label_path)
+    test_color, test_gray, test_ab, test_quantized = __loop_filetree(image_path, test_mat_file['file_list'], size)
+    
+    print('testing done')
 
-def save_images(images, save_path):
+    train_data = {'rgb_images': train_color, 'gray_images': train_gray, 'ab_images': train_ab, 'quantized_images': train_quantized}
+    test_data = {'rgb_images': test_color, 'gray_images': test_gray, 'ab_images': test_ab, 'quantized_images': test_quantized}
+
+    return train_data, test_data
+
+def save_data(images, save_path):
     """
     images: list of numpy arrays
     save_path: exact saving location
 
     summary: saves numpy_images to save_path as numpy array
     """
-    
+
     with open(save_path, 'wb') as f:
         pickle.dump(images, f)
 
-def load_images(save_path):
+def load_data(save_path):
     """
     save_path: exact location of numpy data
 
     summary: loads numpy file from save_path
     """
+
     with open(save_path, 'rb') as f:
         images = pickle.load(f)
     
     return images
 
 if __name__ == "__main__":
-    train_images, test_images = convert_images(os.path.join(DATASET_PATH, 'Images'), os.path.join(DATASET_PATH,'lists/train_list.mat'), os.path.join(DATASET_PATH,'lists/test_list.mat'))
-    print(len(train_images), len(test_images)) 
+    train_data, test_data = convert_images(os.path.join(DATASET_PATH, 'Images'), os.path.join(DATASET_PATH,'lists/train_list.mat'), os.path.join(DATASET_PATH,'lists/test_list.mat'))
 
-    save_images(train_images, os.path.join(PICKLE_PATH, 'train.pkl'))
-    save_images(test_images, os.path.join(PICKLE_PATH, 'test.pkl'))
+    save_data(train_data, os.path.join(PICKLE_PATH, 'train_data.pkl'))
+    save_data(test_data, os.path.join(PICKLE_PATH, 'test_data.pkl'))
+
+    train_data = load_data(os.path.join(PICKLE_PATH, 'train_data.pkl'))
+    test_data = load_data(os.path.join(PICKLE_PATH, 'test_data.pkl'))
+
+    print(train_data['rgb_images'].shape)
+    print(train_data['gray_images'].shape)
+    print(train_data['ab_images'].shape)
+    print(train_data['quantized_images'].shape)
+
+    print(test_data['rgb_images'].shape)
+    print(test_data['gray_images'].shape)
+    print(test_data['ab_images'].shape)
+    print(test_data['quantized_images'].shape)
